7d23cc95d958f7c2a6e6d58fb95abc664d98c62f
Vasyl <vasyl@voicett.com>
2023-09-26 18:30:31 +0300

    add one more example.

--- index.ts
+++ index.ts
@@ -81,4 +81,70 @@ async function main() {
   }
 }
 
-main();
+// main();
+
+// Define the user management sub-schema
+class UserManagementOther {
+  @prop({ _id: false, set: () => { return {} } })
+  permissions_management?: Record<string, never>;
+
+  @prop({ _id: false, set: () => { return {} } })
+  acc_permission?: Record<string, never>;
+
+  @prop({ _id: false, set: () => { return {} } })
+  qaa_permission?: Record<string, never>;
+}
+
+// Define the main schema
+class UsersPermissionsOther {
+  @prop({ _id: false, set: () => { return {} } })
+  superAdmin?: Record<string, never>;
+
+  @prop({ _id: false, set: () => { return {} } })
+  settings?: Record<string, never>;
+
+  @prop({ _id: false, type: () => UserManagementOther })
+  user_management?: UserManagementOther;
+}
+
+const UserPermissionsModelOther = getModelForClass(UsersPermissionsOther, {
+  schemaOptions: {
+    minimize: false,
+  }
+});
+
+async function mainOther() {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/', {dbName: 'test-empty-object-other'});
+
+    // will not fail, but user_management is an empty object with missing permissions_management, acc_permission, ...
+    const uP = new UserPermissionsModelOther({
+      superAdmin: {
+        a: 1
+      },
+      settings: {
+        b: 2
+      },
+      user_management: {
+        permissions_management: {
+          a: 1
+        },
+        acc_permission: {
+          b: 1
+        },
+        qaa_permission: {
+          a: 'v'
+        }
+      }
+    })
+
+    await uP.save();
+
+    process.exit(0);
+  } catch (err) {
+    console.log('err', err);
+    process.exit(1);
+  }
+}
+
+mainOther();
